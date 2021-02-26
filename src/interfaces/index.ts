0cfc722037ca32a7927831edef657e7d84fdf09d
Igor Santos <igornzds@gmail.com>
2021-02-26 13:05:46 -0300

    update interface with InitialMarker and IAllUnits

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,33 @@ export interface HeaderProps {
   title: string;
   showCancel?: boolean;
 }
+
+export interface InitialMarker {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+}
+
+export interface IAllUnits {
+  id: number;
+  name: string;
+  country: string;
+  state: string;
+  city: string;
+  profile: string;
+  latitude: number;
+  longitude: number;
+  address: {
+    street: string;
+    complement: string;
+    number: string;
+    zipCode: string;
+  };
+  describle: string;
+  contactURL: string;
+}
+
+export interface GetUnit {
+  id: number;
+}
